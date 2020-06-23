8314e948f4b3ea18b0db00c2bbf4b0cbc7fbb2f4
Gianluca Daffré <gianluca.daffre@noseryoung.ch>
2020-06-23 14:00:38 +0200

    Bugfix

--- app/src/main/java/com/example/pendenzen_verwaltung/CreateNewTodoActivity.java
+++ app/src/main/java/com/example/pendenzen_verwaltung/CreateNewTodoActivity.java
@@ -7,20 +7,35 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class CreateNewTodoActivity extends AppCompatActivity {
 
+    RadioGroup rgPriority;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_new_todo);
+        rgPriority = findViewById(R.id.priorityRadioGroup);
     }
 
 
     public void saveNewTodo(View v){
+        int selectedRadioButtonID = rgPriority.getCheckedRadioButtonId();
+        int prioValue = 0;
+        switch (selectedRadioButtonID) {
+            case R.id.lowPriorityRadioButton:
+                prioValue = 0;
+            case R.id.mediumPriorityRadioButton:
+                prioValue = 1;
+            case R.id.highPriorityRadioButton:
+                prioValue = 2;
+        }
+
         List<Pending> todoToSave = new ArrayList<>();
         Pending pending = new Pending();
         EditText title = findViewById(R.id.titleInput);
