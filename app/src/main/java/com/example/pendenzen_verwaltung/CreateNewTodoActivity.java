fbde4714144cf8039ec700a523721d500665dec2
Joel Oppliger <joel.oppliger@noseryoung.ch>
2020-06-23 15:06:23 +0200

    Added description

--- app/src/main/java/com/example/pendenzen_verwaltung/CreateNewTodoActivity.java
+++ app/src/main/java/com/example/pendenzen_verwaltung/CreateNewTodoActivity.java
@@ -1,12 +1,10 @@
 package com.example.pendenzen_verwaltung;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.DialogFragment;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 import java.util.ArrayList;
@@ -16,15 +14,20 @@ public class CreateNewTodoActivity extends AppCompatActivity {
 
     RadioGroup rgPriority;
 
+    PendingDao mPendingDao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_new_todo);
         rgPriority = findViewById(R.id.priorityRadioGroup);
+        mPendingDao = AppDatabase.getAppDb(getApplicationContext()).getPendingDao();
     }
 
 
     public void saveNewTodo(View v){
+
+
+
         int selectedRadioButtonID = rgPriority.getCheckedRadioButtonId();
         int prioValue = 0;
         switch (selectedRadioButtonID) {
@@ -39,10 +42,25 @@ public class CreateNewTodoActivity extends AppCompatActivity {
         List<Pending> todoToSave = new ArrayList<>();
         Pending pending = new Pending();
         EditText title = findViewById(R.id.titleInput);
-        pending.setPendingTitle(title.getText().toString());
+        EditText description = findViewById(R.id.descriptionInput);
         EditText date = findViewById(R.id.dateInput);
-        pending.setPendingTitle(date.getText().toString());
 
+        String titleText = title.getText().toString();
+        String descriptionText = description.getText().toString();
+        String dateText = date.getText().toString();
+        if(!(titleText == "" || descriptionText == "" || dateText == "")){
+            pending.setPendingTitle(titleText);
+            pending.setPendingDescription(descriptionText);
+            pending.setPendingTitle(dateText);
+            pending.setPendingImportance(prioValue);
 
+            mPendingDao.insertAll(todoToSave);
+
+            title.setText("");
+            description.setText("");
+            date.setText("");
+        }else{
+            
+        }
     }
 }
