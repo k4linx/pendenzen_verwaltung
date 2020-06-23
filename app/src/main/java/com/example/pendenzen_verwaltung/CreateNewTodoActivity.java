4db754d66bae659b46d77675f8618220241ead0d
Joel Oppliger <joel.oppliger@noseryoung.ch>
2020-06-23 13:31:34 +0200

    WIP radiobuttons

--- app/src/main/java/com/example/pendenzen_verwaltung/CreateNewTodoActivity.java
+++ app/src/main/java/com/example/pendenzen_verwaltung/CreateNewTodoActivity.java
@@ -5,6 +5,11 @@ import androidx.fragment.app.DialogFragment;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CreateNewTodoActivity extends AppCompatActivity {
 
@@ -12,5 +17,17 @@ public class CreateNewTodoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_new_todo);
+    }
+
+
+    public void saveNewTodo(View v){
+        List<Pending> todoToSave = new ArrayList<>();
+        Pending pending = new Pending();
+        EditText title = findViewById(R.id.titleInput);
+        pending.setPendingTitle(title.getText().toString());
+        EditText date = findViewById(R.id.dateInput);
+        pending.setPendingTitle(date.getText().toString());
+
+
     }
 }
